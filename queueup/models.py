002f6710bb0fc832ead7f5f024437f0f981b47ad
rahilshah13 <rahil@vt.edu>
2020-12-18 19:03:50 -0500

    requirements.txt

--- queueup/models.py
+++ queueup/models.py
@@ -5,10 +5,11 @@ from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    id_ = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    picture = db.Column(db.String(20), nullable=False, default='default.jpg')
+
 
     # profile info fill in later
     # posts = db.relationship('Post', backref='author', lazy=True)
--- queueup/router.py
+++ queueup/router.py
@@ -47,20 +47,59 @@ def authorized():
     token_endpoint = google_provider_cfg["token_endpoint"]
 
     # Prepare and send a request to get tokens! Yay tokens!
-    token_url, headers, body = oauth.prepare_token_request(token_endpoint,
+    token_url, headers, body = oauth.prepare_token_request(
+        token_endpoint,
         authorization_response=request.url,
         redirect_url=request.base_url,
         code=code
     )
-    token_response = requests.post(token_url, headers=headers, data=body,
-                                   auth=(config['GOOGLE_CLIENT_ID'], config['GOOGLE_CLIENT_SECRET']),
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(config['GOOGLE_CLIENT_ID'], config['GOOGLE_CLIENT_SECRET']),
     )
 
     # Parse the tokens!
     oauth.parse_request_body_response(json.dumps(token_response.json()))
 
+    # Now that you have tokens let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = oauth.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
 
 
+    # verify their email through Google
+    if userinfo_response.json().get("email_verified"):
+        id = userinfo_response.json()["sub"]
+        email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in db with the information provided by Google
+    user = User(
+        id_=id, name=name, email=email, picture=picture
+    )
+
+    # Doesn't exist? Add it to the database.
+    if not User.query.get(id):
+        User.create(id, name, email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
 
 # @app.route("/create_profile", methods=['POST'])
 # def create_profile():
@@ -85,10 +124,6 @@ def authorized():
 #     return
 
 
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("index"))
+
 
 
